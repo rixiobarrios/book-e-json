ac87541660ada948df86f44cc37ca11e4f8c6d75
Hammad Malik <tomatohammado@gmail.com>
2019-05-31 08:15:03 -0400

    Add options to Update methods

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // import the bookmark model
-const Bookmark = require("../db/models/Bookmark")
+const Bookmark = require("../db/models/Bookmark");
 
 router.get("/", (req, res) => {
   // option 1, just send some html back
@@ -13,8 +13,8 @@ router.get("/", (req, res) => {
 
   // option 3, use the model to look up data in mongodb
   // send back the data as json
-  Bookmark.find({}).then(allBookmarks => res.json(allBookmarks))
-})
+  Bookmark.find({}).then(allBookmarks => res.json(allBookmarks));
+});
 
 router.get("/:title", (req, res) => {
   // option 1
@@ -26,11 +26,11 @@ router.get("/:title", (req, res) => {
   Bookmark
     // express automatically populates req.params for us
     .find({ title: req.params.title })
-    .then(bookmarks => res.json(bookmarks))
-})
+    .then(bookmarks => res.json(bookmarks));
+});
 
 router.post("/", (req, res) => {
-  let newBookmark = req.body
+  let newBookmark = req.body;
   // option 1
   // console log the request body
   // console.log(newBookmark)
@@ -41,25 +41,25 @@ router.post("/", (req, res) => {
   // create a bookmark from the contents of the body
   Bookmark.create(req.body)
     // send the new record back as json
-    .then(bookmark => res.json(bookmark))
-})
+    .then(bookmark => res.json(bookmark));
+});
 
 router.put("/:title", (req, res) => {
   // look up the bookmark by title
   // use .findOneAndUpdate to find the title
   // update with the new values passed in from request body
 
-  Bookmark.findOneAndUpdate({ title: req.params.title }, req.body).then(
-    bookmark => res.json(bookmark)
-  )
-})
+  Bookmark.findOneAndUpdate({ title: req.params.title }, req.body, {
+    new: true
+  }).then(bookmark => res.json(bookmark));
+});
 
 router.delete("/:title", (req, res) => {
   // use title as a param to find the record we want to remove
   Bookmark.findOneAndDelete({ title: req.params.title }).then(bookmark => {
     // send back the deleted document
-    res.json(bookmark)
-  })
-})
+    res.json(bookmark);
+  });
+});
 
-module.exports = router
+module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,29 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // import the user model
-const User = require("../db/models/User")
-const Bookmark = require("../db/models/Bookmark")
+const User = require("../db/models/User");
+const Bookmark = require("../db/models/Bookmark");
 
 router.get("/", (req, res) => {
   User.find().then(allUsers => {
-    res.json(allUsers)
-  })
-})
+    res.json(allUsers);
+  });
+});
 
 router.get("/:email", (req, res) => {
-  let userEmail = req.params.email
+  let userEmail = req.params.email;
   User.findOne({ email: userEmail }).then(user => {
-    res.json(user)
-  })
-})
+    res.json(user);
+  });
+});
 
 router.post("/", (req, res) => {
-  let newUser = req.body
+  let newUser = req.body;
   User.create(newUser).then(created => {
-    res.json(created)
-  })
-})
+    res.json(created);
+  });
+});
 
 // in this case, we will use two models and the refs to link them
 // sample body payload:
@@ -45,23 +45,23 @@ router.post("/new", (req, res) => {
   User.create(req.body.user).then(newUser => {
     Bookmark.create(req.body.bookmark).then(newBookmark => {
       // push new bookmark id into user.favorites array
-      newUser.favorites.push(newBookmark._id)
+      newUser.favorites.push(newBookmark._id);
       // push new user id into bookmark.favorited array
-      newBookmark.favorited.push(newUser._id)
+      newBookmark.favorited.push(newUser._id);
 
       // save both or they wont persist
-      newUser.save()
-      newBookmark.save()
+      newUser.save();
+      newBookmark.save();
       // send entire document back
-      res.json(newUser)
-    })
-  })
-})
+      res.json(newUser);
+    });
+  });
+});
 
 // favorite a bookmark by a user id
 router.put("/:id/:bookmarkId", (req, res) => {
-  let userID = req.params.id
-  let bookmarkID = req.params.bookmarkId
+  let userID = req.params.id;
+  let bookmarkID = req.params.bookmarkId;
 
   // find the bookmark by its id
   Bookmark.findById(bookmarkID).then(mark => {
@@ -69,28 +69,30 @@ router.put("/:id/:bookmarkId", (req, res) => {
     // could also swap this out with email
     User.findOneAndUpdate({ _id: userID }).then(user => {
       // push each id into the others array
-      user.favorites.push(mark._id)
-      mark.favorited.push(user._id)
+      user.favorites.push(mark._id);
+      mark.favorited.push(user._id);
       // save both
-      user.save()
-      mark.save()
+      user.save();
+      mark.save();
 
       // send json response
-      res.json(user)
-    })
-  })
-})
+      res.json(user);
+    });
+  });
+});
 
 router.put("/:id", (req, res) => {
-  User.findOneAndUpdate({ _id: req.params.id }, req.body).then(prevRecord => {
-    res.json(prevRecord)
-  })
-})
+  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(
+    prevRecord => {
+      res.json(prevRecord);
+    }
+  );
+});
 
 router.delete("/:id", (req, res) => {
   User.findOneAndDelete({ _id: req.params.id }).then(deleted => {
-    res.json(deleted)
-  })
-})
+    res.json(deleted);
+  });
+});
 
-module.exports = router
+module.exports = router;
