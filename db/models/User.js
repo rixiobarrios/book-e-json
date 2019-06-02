7f53b6cdee5942debaa4949a0b721e18e086fe04
Hammad Malik <tomatohammado@gmail.com>
2019-06-02 19:28:28 -0400

    Prettier normalization

--- db/models/User.js
+++ db/models/User.js
@@ -1,14 +1,16 @@
-const mongoose = require('../connection')
+const mongoose = require("../connection");
 
 const UserSchema = mongoose.Schema({
-	email: String,
-	name: String,
-	favorites: [{
-		ref: "Bookmark",
-		type: mongoose.Schema.Types.ObjectId
-	}]
-})
+  email: String,
+  name: String,
+  favorites: [
+    {
+      ref: "Bookmark",
+      type: mongoose.Schema.Types.ObjectId
+    }
+  ]
+});
 
-let user = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema);
 
-module.exports = user
+module.exports = User;
