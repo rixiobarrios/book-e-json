e19ad9cbc484894b7c0b43807e595b2bdbced4b6
Hammad Malik <tomatohammado@gmail.com>
2019-09-06 14:31:34 -0400

    Implement "Index" and "Show" for bookmarks

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -1,3 +1,14 @@
 const Bookmark = require("../models/Bookmark");
 
-module.exports = {};
+module.exports = {
+  index: (req, res) => {
+    Bookmark.find({}).then(bookmarks => {
+      res.json(bookmarks);
+    });
+  },
+  show: (req, res) => {
+    Bookmark.findOne({ title: req.params.title }).then(bookmark =>
+      res.json(bookmark)
+    );
+  }
+};
--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -5,4 +5,12 @@ const bookmarksController = require("../controllers/bookmarks");
 
 // add my routers here
 
+/** Index: show all bookmarks */
+
+bookmarksRouter.get("/", bookmarksController.index);
+
+/** Show: retreive one bookmakr by its title */
+
+bookmarksRouter.get("/:title", bookmarksController.show);
+
 module.exports = bookmarksRouter;
